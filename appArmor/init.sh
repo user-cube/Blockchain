b78c3420d36b597f337b8fe6c51f6bcce9ae1268
Rui <33549232+user-cube@users.noreply.github.com>
2019-07-21 15:43:25 +0100

    Add files via upload

---
+++ appArmor/init.sh
@@ -0,0 +1,6 @@
+sudo mkdir -p /opt/chroot &&
+sudo mkdir -p /opt/chroot/bin &&
+sudo mkdir -p /opt/chroot/lib &&
+sudo mkdir -p /opt/chroot/lib64 &&
+sudo mkdir -p /opt/chroot/data &&
+sudo mkdir -p /opt/chroot/data
